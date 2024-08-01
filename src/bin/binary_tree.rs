5bac9c7446b605dca04c322e6ca0e46c858a195e
Ppjamies <pj.jindrich@gmail.com>
2024-08-01 16:51:12 -0700

    moved binary_tree.rs and added test function.

--- src/bin/binary_tree.rs
+++ src/bin/binary_tree.rs
@@ -82,3 +82,21 @@ impl<T> Node<T> {
         println!("({:?})", self.val);
     }
 }
+
+#[test]
+fn binary_tree_test() {
+    let mut root = Node::new(10);
+    root.set_left(Node::new(5));
+    root.set_right(Node::new(15));
+
+    println!("arr {:#?}\n", root);
+
+    println!("Pre-order traversal...");
+    root.preorder_visit();
+
+    println!("In-order traversal...");
+    root.inorder_visit();
+
+    println!("Post-order traversal...");
+    root.postorder_visit();
+}
