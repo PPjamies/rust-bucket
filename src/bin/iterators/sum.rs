0fb855c5500e6e3e7346c4546f3c271d89dbab21
pjamies <pj.jindrich@gmail.com>
2024-07-31 23:01:42 -0700

    created sum.rs to learn about iterators.

--- src/bin/iterators/sum.rs
+++ src/bin/iterators/sum.rs
@@ -1,4 +1,12 @@
-pub fn itr_sum() {
-    let numbers = vec![1, 2, 3];
+fn itr_sum(numbers: &Vec<i32>) {
     numbers.iter().sum(); // consumes the iterator
 }
+
+
+#[test]
+fn itr_sum_test() {
+    let numbers = vec![1, 2, 3];
+    assert_eq!(6, itr_sum(numbers));
+}
+
+fn main() {}
